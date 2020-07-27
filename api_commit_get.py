d3f5fa5d6c94175cb555e899d0f33f37f52ce0bf
Nick Zeng <zenghongyi2004@gmail.com>
2020-07-26 22:49:41 -0700

    Fix bug that can cause pointer can't write into dict when last pointer isn't exit.

--- api_commit_get.py
+++ api_commit_get.py
@@ -277,10 +277,15 @@ def commit_info(continue_mode_enable, video_oid, commit_all, commit_index, reply
                         if timestep_key_dire:
                             last_time_step_user_dire = all_user_full_timestep_dict[str(
                                 last_time_step)]
-                        last_time_step_user_dire = last_time_step_user_dire[member_id]
+                        last_time_step_user_dire = last_time_step_user_dire[str(member_id)]
                     else:
-                        last_time_step_user_dire = last_time_step_user_dire[member_id]
-                    if last_time_step_user_dire[key] == commit_user_info[key]:
+                        if str(member_id) in last_time_step_user_dire.keys():
+                            last_time_step_user_dire = last_time_step_user_dire[str(member_id)]
+                            found_old_data = True
+                        else:
+                            print('Can not found old data for '+key+' data in user_dict, creating new record')
+                            found_old_data = False
+                    if last_time_step_user_dire[key] == commit_user_info[key] and found_old_data:
                         commit_user_info[key] = {
                             'last_time_step_pointer': last_time_step}
 
@@ -344,6 +349,13 @@ def commit_info(continue_mode_enable, video_oid, commit_all, commit_index, reply
                             last_commit_dire = all_commit_full_timestep_dict[str(
                                 last_time_step)]
                         last_commit_dire = last_commit_dire[reply_id]
+                    else:
+                        if str(reply_id) in last_time_step_comment_dire.keys():
+                            last_commit_dire = last_time_step_comment_dire[str(reply_id)]
+                            found_old_data = True
+                        else:
+                            found_old_data = False
+                            print('Can not found old data for '+key+' data in commit_dict, creating new record')
                     if last_commit_dire[key] == commit_info[key]:
                         print('Find same data for '+key+' , wrting pointer to it.')
                         commit_info[key] = {
