0d154f9149d53f2f07524c50a230f60efc49ec15
Nick Zeng <zenghongyi2004@gmail.com>
2020-08-05 19:43:39 -0700

    Reglion model now is trainable, for futer

--- deeplearning/fake-gpt3.py
+++ deeplearning/fake-gpt3.py
@@ -6,36 +6,48 @@ from keras import callbacks
 import os
 import json
 import numpy as np
+from tqdm import tqdm
 import numba as nb
+
+NUMBA_WARNINGS = 0
 
 def gpt_dataset_builder(comment_data_dict):
     proccess_index = 0
     target_list_sort = []
     up_list_sort = []
     down_list_sort = []
-    current_down_list = []
-    current_up_list = []
+    
     display_count = len(list(comment_data_dict.keys()))
     display_show = 1 
     for reply_id in list(comment_data_dict.keys()):
         # print('Working on '+str(display_show)+'/'+str(display_count))
         current_encoded_message = comment_data_dict[reply_id]
         while proccess_index < len(current_encoded_message):
+            current_down_list = []
+            current_up_list = []
             if proccess_index == 0:
                 current_up_list = [0]
+            
             else:
-                for up_index in range(0, proccess_index - 1):
+                up_index = 0
+                while up_index == proccess_index - 1 or up_index < proccess_index - 1:
                     current_up_list.append(current_encoded_message[up_index])
+                    up_index += 1
             if proccess_index == len(current_encoded_message) - 1 :
+                # TODO: Test wo delete it 
                 current_down_list = [0]
             else:
-                for down_index in range(proccess_index + 1, len(current_encoded_message) - 1):
+                # TODO: Replace with a while loop
+                down_index = 0
+                while (down_index == proccess_index + 1 or down_index > proccess_index) and (down_index < len(current_encoded_message) - 1 or down_index == len(current_encoded_message) - 1):
                     current_down_list.append(current_encoded_message[down_index])
+            
             target_list_sort.append(current_encoded_message[proccess_index])
             up_list_sort.append(current_up_list)
             down_list_sort.append(current_down_list)
             proccess_index += 1
             display_show += 1
+        proccess_index = 0
     return target_list_sort, up_list_sort, down_list_sort
 
 def random_up(target_list_sort, up_list_sort, down_list_sort):
@@ -95,18 +107,22 @@ def split_dataset(percent_of_transet, testset_present,target_list_rand, up_list_
     return train_up_list, test_up_list ,train_down_list, test_down_list, train_target_list, test_target_list
 
 
-def build_reglaiour_model(max_index_up_text):
+def build_reglaiour_model(max_index_up_text, maxium_train_up_legth):
     down_text_tensor = 0
-    up_text = Input(shape=(None,), name='up_text')
+    up_text = Input(shape=(None, maxium_train_up_legth), name='up_text')
     
 
     up_text_emb = layers.Embedding(max_index_up_text, 64)(up_text)
 
-    cnn_up_text_1 = layers.Conv1D(500,1)(up_text_emb)
-    cnn_up_text_2 = layers.Conv1D(500,2)(up_text_emb)
-    cnn_up_text_3 = layers.Conv1D(500,4)(up_text_emb)
+    cnn_up_text_1 = layers.Conv1D(500,1, padding='same')(up_text_emb)
+    cnn_up_text_2 = layers.Conv1D(500,2, padding='same')(up_text_emb)
+    cnn_up_text_3 = layers.Conv1D(500,4, padding='same')(up_text_emb)
+
+    
 
     cnn_up_output = layers.add([cnn_up_text_1, cnn_up_text_2, cnn_up_text_3])
+    cnn_up_output = layers.Reshape((maxium_train_up_legth, 500))(cnn_up_output)
+  
     
     
 
@@ -117,14 +133,17 @@ def build_reglaiour_model(max_index_up_text):
 
     
 
-    down_text_tensor = Input(shape=(None,),  name='down_text')
+    down_text_tensor = Input(shape=(None,maxium_train_up_legth),  name='down_text')
 
     down_text_tensor_emb = layers.Embedding(max_index_up_text, 64)(down_text_tensor)
-    cnn_down_output_1 = layers.Conv1D(500,1)(down_text_tensor_emb)
-    cnn_down_output_2 = layers.Conv1D(500,2)(down_text_tensor_emb)
-    cnn_down_output_3 = layers.Conv1D(500,3)(down_text_tensor_emb)
+    cnn_down_output_1 = layers.Conv1D(500,1,padding='same')(down_text_tensor_emb)
+    cnn_down_output_2 = layers.Conv1D(500,2,padding = 'same')(down_text_tensor_emb)
+    cnn_down_output_3 = layers.Conv1D(500,3, padding = 'same')(down_text_tensor_emb)
 
     cnn_down_output = layers.add([cnn_down_output_1, cnn_down_output_2, cnn_down_output_3])
+    cnn_down_output = layers.Reshape((maxium_train_up_legth, 500))(cnn_down_output)
+   
+
     lstm_down_output = layers.LSTM(600, return_sequences=True)(cnn_down_output)
     lstm_down_output = layers.LSTM(500, return_sequences=True)(lstm_down_output)
     lstm_down_output = layers.LSTM(400, return_sequences=True)(lstm_down_output)
@@ -165,9 +184,7 @@ except:
 enc_dict = json.load(enc_dict_file)
 encode_comment_dict = json.load(encode_comment_dict_file)
 
-print("building model")
-max_enc_index = len(list(enc_dict.keys())) - 1
-model = build_reglaiour_model(max_enc_index)
+
 
 print("Inirliazing dataset...")
 target_list_sort, up_list_sort, down_list_sort = gpt_dataset_builder(encode_comment_dict)
@@ -195,6 +212,11 @@ test_up_count = len(test_up_list)
 test_down_count = len(test_down_list)
 test_target_count = len(test_target_list)
 
+print("building model")
+max_enc_index = len(list(enc_dict.keys())) - 1
+model = build_reglaiour_model(max_enc_index, maxium_train_up_legth)
+print(model.summary())
+
 print("Initlazing up array...")
 train_up_arry = np.zeros((train_up_count,maxium_train_up_legth),dtype=np.float32)
 print(train_up_arry)
@@ -208,31 +230,52 @@ test_down_arry = np.zeros((test_down_count,maxium_test_down_legth),dtype=np.floa
 
 
 print("transforming list to numpy array...")
-@nb.jit(nopython=True, parallel=True)
-def train_up_arry_trans(train_up_list):
-    for splet_index, train_spelt in enumerate(train_up_list):
+@nb.jit 
+def up_trans_train_arrary():
+    for splet_index, train_spelt in tqdm(enumerate(train_up_list), total=len(train_up_list)):
         for charater_index, charater in enumerate(train_spelt):
             train_up_arry[splet_index, charater_index] = charater
-    return train_up_arry
+up_trans_train_arrary()
 print(train_up_arry)
-for splet_index, train_spelt in enumerate(train_target_list):
-        train_target_arry[splet_index,0] = train_spelt
-    
-for train_spelt in train_down_list:
-    np.append(train_down_arry,np.array(train_spelt, dtype=np.int32))
 
-for test_spelt in test_up_list:
-    np.append(test_up_arry,np.array(test_spelt, dtype=np.int32))
-for test_spelt in test_target_list:
-    np.append(test_target_arry,np.array(test_spelt, dtype=np.int32))
-for test_spelt in test_down_list:
-    np.append(test_down_arry,np.array(test_spelt, dtype=np.int32))
+@nb.jit
+def target_trans_train_arrary():
+    for splet_index, train_spelt in tqdm(enumerate(train_target_list), total=len(train_target_list)):
+            train_target_arry[splet_index,0] = train_spelt
+target_trans_train_arrary()
+
+@nb.jit
+def down_trans_train_arrary():
+    for splet_index, train_spelt in tqdm(enumerate(train_down_list), total=len(train_down_list)):
+        for charater_index, charater in enumerate(train_spelt):
+            train_down_arry[splet_index, charater_index] = charater
+down_trans_train_arrary()
+
+@nb.jit
+def up_trans_test_arrary():
+    for splet_index, test_spelt in tqdm(enumerate(test_up_list), total=len(test_up_list)):
+        for charater_index, charater in enumerate(test_spelt):
+            test_up_arry[splet_index, charater_index] = charater
+up_trans_test_arrary()
+
+@nb.jit
+def target_trans_test_arrary():
+    for splet_index, test_spelt in tqdm(enumerate(test_target_list), total=len(test_target_list)):
+        test_target_arry[splet_index,0] = test_spelt
+target_trans_test_arrary()
+
+@nb.jit
+def down_trans_test_arrary():
+    for splet_index, test_spelt in tqdm(enumerate(test_down_list), total=len(test_down_list)):
+        for charater_index, charater in enumerate(test_spelt):
+            test_down_arry[splet_index, charater_index] = charater
+down_trans_test_arrary()
 
 
 
 print("Starting fitting model...")
 tensor_callback = callbacks.TensorBoard(log_dir = './tesorboard', histogram_freq= 1, embeddings_freq= 1)
-model.fit({'up_text': train_up_arry, 'down_text': train_down_arry}, train_target_arry, verbose= 1 , callbacks= tensor_callback, batch_size=1, epochs= 100, validation_split = 0.2)
+model.fit({'up_text': train_up_arry, 'down_text': train_down_arry}, train_target_arry, verbose= 1 , callbacks= tensor_callback, epochs= 100, batch_size=1)
 
 
 
