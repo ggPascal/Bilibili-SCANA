c95a758fa1d5ef80465c33e08aea18de33c7eda0
Nick Zeng <zenghongyi2004@gmail.com>
2020-08-07 14:43:37 -0700

    Fix a problem that missing some vaule when loading from file and formart code, decrease the epochs

--- deeplearning/fake-gpt3.py
+++ deeplearning/fake-gpt3.py
@@ -120,45 +120,43 @@ def split_dataset(percent_of_transet, testset_present, target_list_rand, up_list
 
 def build_reglaiour_model(max_index_up_text, maxium_legth):
 
-    
-
-    up_text = Input(shape=(None, maxium_legth), name='up_text', dtype='float32')
+    up_text = Input(shape=(None, maxium_legth),
+                    name='up_text', dtype='float32')
 
     up_text_emb = layers.Embedding(max_index_up_text + 1, 64)(up_text)
 
-    cnn_up_text_1 = layers.Conv1D(700, 1, padding='same')(up_text_emb)
-    cnn_up_text_2 = layers.Conv1D(700, 2, padding='same')(up_text_emb)
-    cnn_up_text_3 = layers.Conv1D(700, 4, padding='same')(up_text_emb)
+    cnn_up_text_1 = layers.Conv1D(500, 1, padding='same')(up_text_emb)
+    cnn_up_text_2 = layers.Conv1D(500, 2, padding='same')(up_text_emb)
+    cnn_up_text_3 = layers.Conv1D(500, 4, padding='same')(up_text_emb)
 
     cnn_up_output = layers.add([cnn_up_text_1, cnn_up_text_2, cnn_up_text_3])
-    cnn_up_output = layers.Reshape((maxium_legth, 700))(cnn_up_output)
+    cnn_up_output = layers.Reshape((maxium_legth, 500))(cnn_up_output)
 
-    lstm_up_output = layers.LSTM(800, return_sequences=True)(cnn_up_output)
-   
+    lstm_up_output = layers.LSTM(600, return_sequences=True)(cnn_up_output)
+
     down_text_tensor = Input(
         shape=(None, maxium_legth),  name='down_text', dtype='float32')
 
     down_text_tensor_emb = layers.Embedding(
         max_index_up_text + 1, 64)(down_text_tensor)
     cnn_down_output_1 = layers.Conv1D(
-        700, 1, padding='same')(down_text_tensor_emb)
+        500, 1, padding='same')(down_text_tensor_emb)
     cnn_down_output_2 = layers.Conv1D(
-        700, 2, padding='same')(down_text_tensor_emb)
+        500, 2, padding='same')(down_text_tensor_emb)
     cnn_down_output_3 = layers.Conv1D(
-        700, 3, padding='same')(down_text_tensor_emb)
+        500, 3, padding='same')(down_text_tensor_emb)
 
     cnn_down_output = layers.add(
         [cnn_down_output_1, cnn_down_output_2, cnn_down_output_3])
     cnn_down_output = layers.Reshape(
-        (maxium_legth, 700))(cnn_down_output)
+        (maxium_legth, 500))(cnn_down_output)
 
-    lstm_down_output = layers.LSTM(800, return_sequences=True)(cnn_down_output)
-   
-    
+    lstm_down_output = layers.LSTM(600, return_sequences=True)(cnn_down_output)
+
     lstm_output = layers.concatenate([lstm_up_output, lstm_down_output])
     lstm_output = layers.Flatten()(lstm_output)
 
-    final_output = layers.Dense(400)(lstm_output)
+    final_output = layers.Dense(200)(lstm_output)
 
     model = Model(inputs=[up_text, down_text_tensor], outputs=[final_output])
     model.compile(optimizer='Adadelta', loss='mean_squared_error')
@@ -170,11 +168,11 @@ data_root_dir = 'E:\\爬虫\\test-data\\BV1av411v7E1'
 enc_dict_root_dir = 'E:\\爬虫\\test-data\\dec-enc-dict'
 model_root_path = 'E:\\爬虫\\Fake-GPT3\\models'
 
-make_new_data = False
+make_new_data = True
 make_new_model = True
 
 load_model_data = False
-load_arry_data = True
+load_arry_data = False
 
 model_file_name = None
 
@@ -202,7 +200,7 @@ encode_comment_dict = json.load(encode_comment_dict_file)
 if load_arry_data:
     try:
         os.chdir(data_root_dir)
-        all_in_one_data=np.load('data_train_test_all_in_one.npz')
+        all_in_one_data = np.load('data_train_test_all_in_one')
     except:
         print("Could not read numpy data file")
         make_new_data = True
@@ -215,12 +213,17 @@ if load_arry_data:
         test_down_arry = all_in_one_data['test_down_arry']
         test_target_arry = all_in_one_data['test_target_arry']
 
+        if make_new_model:
+            maxium_legth = all_in_one_data['maxium_legth']
+            max_enc_index = all_in_one_data['max_enc_index']
+
 if load_model_data:
     try:
         os.chdir(model_root_path)
         model = load_model(model_file_name)
     except:
-        print("Could not load model from " + model_file_name + "in "+ model_root_path)
+        print("Could not load model from " +
+              model_file_name + "in " + model_root_path)
 if make_new_data:
     print("Inirliazing dataset...")
     target_list_sort, up_list_sort, down_list_sort = gpt_dataset_builder(
@@ -254,9 +257,6 @@ if make_new_data:
     maxium_legth = max([maxium_train_up_legth, maxium_train_down_legth]) + 1
     max_enc_index = len(list(enc_dict.keys())) - 1
 
-    
-
-
     print("Initlazing up array...")
     train_up_arry = np.zeros(
         (train_up_count, maxium_legth), dtype=np.float32)
@@ -273,84 +273,79 @@ if make_new_data:
     test_down_arry = np.zeros(
         (test_down_count, maxium_legth), dtype=np.float32)
 
-
     print("transforming list to numpy array...")
-
 
     @nb.jit
     def up_trans_train_arrary():
         for splet_index, train_spelt in tqdm(enumerate(train_up_list), total=len(train_up_list)):
             for charater_index, charater in enumerate(train_spelt):
-                train_up_arry[splet_index, charater_index] = charater / max_enc_index
-
+                train_up_arry[splet_index,
+                              charater_index] = charater / max_enc_index
 
     up_trans_train_arrary()
     print(train_up_arry)
-
 
     @nb.jit
     def target_trans_train_arrary():
         for splet_index, train_spelt in tqdm(enumerate(train_target_list), total=len(train_target_list)):
             train_target_arry[splet_index, 0] = train_spelt / max_enc_index
 
-
     target_trans_train_arrary()
-
 
     @nb.jit
     def down_trans_train_arrary():
         for splet_index, train_spelt in tqdm(enumerate(train_down_list), total=len(train_down_list)):
             for charater_index, charater in enumerate(train_spelt):
-                train_down_arry[splet_index, charater_index] = charater / max_enc_index
-
+                train_down_arry[splet_index,
+                                charater_index] = charater / max_enc_index
 
     down_trans_train_arrary()
-
 
     @nb.jit
     def up_trans_test_arrary():
         for splet_index, test_spelt in tqdm(enumerate(test_up_list), total=len(test_up_list)):
             for charater_index, charater in enumerate(test_spelt):
-                test_up_arry[splet_index, charater_index] = charater / max_enc_index
-
+                test_up_arry[splet_index,
+                             charater_index] = charater / max_enc_index
 
     up_trans_test_arrary()
-
 
     @nb.jit
     def target_trans_test_arrary():
         for splet_index, test_spelt in tqdm(enumerate(test_target_list), total=len(test_target_list)):
             test_target_arry[splet_index, 0] = test_spelt / max_enc_index
 
-
     target_trans_test_arrary()
-
 
     @nb.jit
     def down_trans_test_arrary():
         for splet_index, test_spelt in tqdm(enumerate(test_down_list), total=len(test_down_list)):
             for charater_index, charater in enumerate(test_spelt):
-                test_down_arry[splet_index, charater_index] = charater / max_enc_index
-
+                test_down_arry[splet_index,
+                               charater_index] = charater / max_enc_index
 
     down_trans_test_arrary()
-    
-    os.chdir(data_root_dir)
-    np.savez_compressed('data_train_test_all_in_one', train_up_arry = train_up_arry, train_down_arry = train_down_arry, train_target_arry = train_target_arry, test_up_arry = test_up_arry, test_down_arry = test_down_arry, test_target_arry = test_target_arry, max_enc_index = max_enc_index)
 
-   
+    os.chdir(data_root_dir)
+    np.savez_compressed('data_train_test_all_in_one', train_up_arry=train_up_arry, train_down_arry=train_down_arry, train_target_arry=train_target_arry,
+                        test_up_arry=test_up_arry, test_down_arry=test_down_arry, test_target_arry=test_target_arry, max_enc_index=max_enc_index, maxium_legth=maxium_legth)
+
 
 if make_new_model:
     print("building model")
-    
+
     model = build_reglaiour_model(max_enc_index, maxium_legth)
     model.save("E:\\爬虫\\Fake-GPT3\\models\\init.h5")
     print(model.summary())
 print("Starting fitting model...")
 tensor_callback = callbacks.TensorBoard(
     log_dir='E:\\爬虫\\Fake-GPT3\\tensorboard', histogram_freq=1, embeddings_freq=1, update_freq='batch')
-save_checkpoint = callbacks.ModelCheckpoint("E:\\爬虫\\Fake-GPT3\\Check-point\\best_val_loss.h5", monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
-auto_stop = callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
+save_checkpoint = callbacks.ModelCheckpoint("E:\\爬虫\\Fake-GPT3\\Check-point\\best_val_loss.h5",
+                                            monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+
+auto_stop = callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0,
+                                    verbose=0, mode='auto', baseline=None, restore_best_weights=False)
+
 model.fit({'up_text': train_up_arry, 'down_text': train_down_arry},
-          train_target_arry, verbose=1, callbacks=[tensor_callback, save_checkpoint, auto_stop], epochs=100, validation_split = 0.4, batch_size=10)
+          train_target_arry, verbose=1, callbacks=[tensor_callback, save_checkpoint, auto_stop], epochs=100, validation_split=0.4, batch_size=10)
 model.save("E:\\爬虫\\Fake-GPT3\\models\\result.h5")
